7c3592b92d3b8bb65e577049c37dd2d232a6e4fc
lordsilly <magnustait@gmail.com>
2017-08-09 21:35:30 +0100

    Attempting OpenCL implementation. Need to pass everything as 1d arrays. This is going to be fun.

---
+++ FloydWarshallParallel/kernel.cl
@@ -0,0 +1,4 @@
+__kernel void floydwarshall(__global long ** graph, __global long ** result)
+{
+	result[0][0] = 12345;
+}
--- FloydWarshallParallel/main.c
+++ FloydWarshallParallel/main.c
@@ -3,6 +3,12 @@
 #include <limits.h>
 #include <string.h>
 
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif
+
 #define inputfilename "C:/Users/lordsilly/Documents/Visual Studio 2015/Projects/FloydWarshallParallel/directedgraph.txt"
 
 int nodes = 0;
@@ -106,19 +112,83 @@ void printGraph(int ** graph) {
 	}
 }
 
+#define MAX_SOURCE_SIZE 128
+
 int main(void) {
-	//open file
+
+	//populate graph from file
 	FILE * input;
 	fopen_s(&input, inputfilename, "r");
 	if (input == NULL) {
 		printf("No File\n");
 		exit(EXIT_FAILURE);
 	}
-
-	//populate graph
 	long ** graph;
 	graph = populate(input);
-	long ** dumbFWGraph = deepcopy(graph);
-	dumbFW(dumbFWGraph);
-	printGraph(dumbFWGraph);
+	fclose(input);
+
+	//setup opencl
+	cl_device_id device_id = NULL;
+	cl_context context = NULL;
+	cl_command_queue command_queue = NULL;
+	cl_mem memsend = NULL;
+	cl_mem memret = NULL;
+	cl_program program = NULL;
+	cl_kernel kernel = NULL;
+	cl_platform_id platform_id = NULL;
+	cl_uint ret_num_devices;
+	cl_uint ret_num_platforms;
+	cl_int ret;
+
+	long ** openclGraph = deepcopy(graph);
+	long ** openclFWGraph = malloc(nodes * sizeof(long*));
+	for (int i = 0; i < nodes; i++) {
+		graph[i] = malloc(nodes * sizeof(long));
+	}
+	FILE *fp;
+	char fileName[] = "kernel.cl";
+	char *source_str;
+	size_t source_size;
+
+	/* Load the source code containing the kernel*/
+	fopen_s(&fp, fileName, "r");
+	if (!fp) {
+		fprintf(stderr, "Failed to load kernel.\n");
+		exit(1);
+	}
+	source_str = (char*)malloc(MAX_SOURCE_SIZE);
+	source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+	fclose(fp);
+
+	/* Get Platform and Device Info */
+	ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+
+	context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);															/* Create OpenCL context */
+	command_queue = clCreateCommandQueue(context, device_id, 0, &ret);															/* Create Command Queue */
+	memsend = clCreateBuffer(context, CL_MEM_READ_ONLY, 1000 * 1000 * sizeof(long), NULL, &ret);								/* Create Memory Buffers */
+	memret = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 1000 * 1000 * sizeof(long), NULL, &ret);
+	ret = clEnqueueWriteBuffer(command_queue, memsend, CL_TRUE, 0, 1000 * 1000 * sizeof(long), openclGraph, 0, NULL, NULL);
+	program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);			/* Create Kernel Program from the source */
+	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);																/* Build Kernel Program */
+	kernel = clCreateKernel(program, "floydwarshall", &ret);																	/* Create OpenCL Kernel */
+	ret = clSetKernelArg(kernel, 0, 1000 * 1000 * sizeof(long), (void *)&memsend);												/* Set OpenCL Kernel Parameters */
+	ret = clEnqueueTask(command_queue, kernel, 0, NULL, NULL);																	/* Execute OpenCL Kernel */
+	ret = clEnqueueReadBuffer(command_queue, memret, CL_TRUE, 0,1000 * 1000 * sizeof(long), openclFWGraph, 0, NULL, NULL);		/* Copy results from the memory buffer */												
+	printf("%d", openclFWGraph[0][0]);
+
+	/* Finalization */
+	ret = clFlush(command_queue);
+	ret = clFinish(command_queue);
+	ret = clReleaseKernel(kernel);
+	ret = clReleaseProgram(program);
+	ret = clReleaseMemObject(memsend);
+	ret = clReleaseMemObject(memret);
+	ret = clReleaseCommandQueue(command_queue);
+	ret = clReleaseContext(context);
+
+	free(source_str);
+
+	return 0;
+	
 }
